9ddd6e30242c57729cbd9400d11c58380bfce894
kiryutaiyo <taikinniku03@gmail.com>
2022-02-20 23:52:19 +0900

    css conection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    //<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet" media="all">
     @yield('css')
     
 </head>
